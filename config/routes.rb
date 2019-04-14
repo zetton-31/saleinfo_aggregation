d3581d5a029d61867cf3e1e6be9e5cec562385c4
YUSUKE KUBO <sd31.mawhdwhb@gmail.com>
2019-04-14 23:45:41 +0900

    Verify Bulma

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/tweet_search', to: 'tweets#index'
+  get '/tweet_search_bulma_slim', to: 'tweets#index_bulma_slim'
+  get '/tweet_search_bulma_html', to: 'tweets#index_bulma_html'
   get '/tweet_lists/', to: 'tweets#search'
   post '/tweet_lists', to: 'tweets#search'
 
